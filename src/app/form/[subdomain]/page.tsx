077cb97a011799678a0a45daac75893fe63292db
seunghwan12 <sh.kim@teamsparta.co>
2023-12-18 18:17:16 +0900

    conflict 해결

--- src/app/form/[subdomain]/page.tsx
+++ src/app/form/[subdomain]/page.tsx
@@ -1,6 +1,5 @@
-import { headers } from 'next/headers';
+import Instance from '@/services/api/Instance';
 import NotionComponent from '@/components/notion/NotionComponent';
-import pageRelease from '@/services/api/pages/pageRelease';
 import ReleaseWrapper from '@/components/ReleaseWrapper';
 
 interface PageProps {
@@ -9,19 +8,19 @@ interface PageProps {
   };
 }
 export default async function FormPage({ params }: PageProps) {
-  const headerList = headers();
-  let pageDomain = params.subdomain;
-  if (process.env.NODE_ENV !== 'development') {
-    const host = headerList.get('host');
-    pageDomain = host?.split('.')[0] || '';
-  }
-  const response = await pageRelease(pageDomain);
+  const pageDomain = params.subdomain;
+
+  const response = await Instance.get<{
+    statusCode: number;
+    message: string;
+    data: Page;
+  }>(`/api/pages/release/${pageDomain}`);
   const page = response.data.data;
   const font = page.pageFont.type;
 
   return (
     <ReleaseWrapper font={font}>
-      <NotionComponent notionBodyHTML={page.pageDetail.content} />
+      <NotionComponent recordMap={page.pageDetail.content} />
     </ReleaseWrapper>
   );
 }
