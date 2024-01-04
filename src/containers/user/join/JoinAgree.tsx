42046c5da1c449eb3c7e2a4ef30cd51ac4343e4b
uujeen <rkddmlwls2@gmail.com>
2024-01-04 16:12:32 +0900

    Cache 삭제 커밋

--- src/containers/user/join/JoinAgree.tsx
+++ src/containers/user/join/JoinAgree.tsx
@@ -3,7 +3,6 @@
 import React, { useEffect, useState } from 'react';
 import Button from '@/components/basic/Button';
 import RectangleCheckOn from '../../../../public/svg/RectangleCheckOn';
-import RectangleCheckOff from '../../../../public/svg/RectangleCheckOff';
 import { SetStateBoolean } from '@/types/type';
 
 function JoinAgree({ setStateBoolean }: SetStateBoolean) {
@@ -27,8 +26,7 @@ function JoinAgree({ setStateBoolean }: SetStateBoolean) {
 
   return (
     <Button className="flex items-center gap-2.5 self-stretch" onClick={() => handleAgree()}>
-      {agree ? <RectangleCheckOn /> : <RectangleCheckOff />} 전체 개인정보 수집 및 폼플렛 이용약관에
-      동의합니다.
+      {agree && <RectangleCheckOn />} 전체 개인정보 수집 및 폼플렛 이용약관에 동의합니다.
     </Button>
   );
 }
