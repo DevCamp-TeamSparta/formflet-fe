730362ea541e30e6146b26c0b978d48e0e440e22
uujeen <rkddmlwls2@gmail.com>
2024-01-04 19:25:28 +0900

    Fix: Password 재설정 에러 수정

--- src/containers/user/password/EditPasswordTable.tsx
+++ src/containers/user/password/EditPasswordTable.tsx
@@ -1,6 +1,5 @@
 import { useRouter } from 'next/navigation';
 import Button from '@/components/basic/Button';
-import Input from '@/components/basic/Input';
 import MESSAGE from '@/constants/Messages';
 import PATH from '@/constants/path/Path';
 import userPasswordReset from '@/services/api/users/userPasswordReset';
@@ -13,9 +12,12 @@ interface EditPasswordProps {
 export default function EditPasswordTable({ email, isSuccessVerify }: EditPasswordProps) {
   const route = useRouter();
   const handleEditPassword = async () => {
-    const currentEmail = document.querySelector<HTMLInputElement>('input[id="email]')?.value;
+    const currentEmail = document.querySelector<HTMLInputElement>('input[id="email"]')?.value;
     const password = document.querySelector<HTMLInputElement>('input[id="password"]')?.value;
-    if (email === currentEmail && isSuccessVerify) {
+    const checkPassword = document.querySelector<HTMLInputElement>('input[id="checkPassword"]')
+      ?.value;
+
+    if (email === currentEmail && isSuccessVerify && checkPassword === password) {
       const data = {
         email,
         password,
@@ -24,7 +26,7 @@ export default function EditPasswordTable({ email, isSuccessVerify }: EditPasswo
         route.push(PATH.ROUTE.LOGIN);
       });
     } else {
-      alert('이메일을 다시 확인해주세요.');
+      alert('이메일 또는 비밀번호를 다시 확인해주세요.');
     }
   };
   return (
@@ -46,7 +48,8 @@ export default function EditPasswordTable({ email, isSuccessVerify }: EditPasswo
           <label className="b1-bold text-gray-dark-active" htmlFor="checkPassword">
             비밀번호 확인
           </label>
-          <Input
+          <input
+            className="flex w-[504px] h-14 items-center gap-2.5 px-8 py-4 border border-gray-normal-normal text-gray-dark-hover placeholder:text-gray-light-active box-shadow-normal focus:box-active-shadow-normal rounded-lg"
             id="checkPassword"
             type="password"
             placeholder={MESSAGE.JOIN_LOGIN.inputcheckPassword}
